5a1daaa5b4d22457c9fabce19117a04d5c52a1d9
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-04-05 18:10:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 // complete the given function
 
 function palindrome(str){
+	str.toLowerCase();
     let i = 0 ;
 	let n = str.length-1;
 	while(i<n){
-		if(str[i]!==str[n]) return false;
+		if(str[i]!==str[n]) {
+			console.log("false");
+			return ;
+		}
     }
-	return true;
+	console.log("true");
+	return ;
 }
 module.exports = palindrome
